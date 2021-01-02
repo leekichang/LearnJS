87363fbdd747297ecdf7e8f0dde4a213f7f2c578
leekichang <1kichang@yonsei.ac.kr>
2021-01-03 03:46:57 +0900

    Modifying DOM

--- 2-3/index.js
+++ 2-3/index.js
@@ -1,5 +1,9 @@
+//DOM: Document Object Model
 const title = document.getElementById("title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "Studying JS";
 console.log(title);
+console.dir(title);
 
-//DOM: Document Object Module
-title.innerHTML = "Hi! From JS"
+
