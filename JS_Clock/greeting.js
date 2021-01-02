9126be8c3e7f5c8c2bfdc825c936365e52f722e5
leekichang <1kichang@yonsei.ac.kr>
2021-01-03 06:02:48 +0900

    saving user's name on local storage

--- JS_Clock/greeting.js
+++ JS_Clock/greeting.js
@@ -5,23 +5,39 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_ON = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handelSubmit (event) {
+    event.preventDefault();
+    const currentValue =  input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_ON);
+    form.addEventListener("submit", handelSubmit);
+}
+
 function paintGreeting(text){
-    form.classList.remove(SHOWING_ON)
+    form.classList.remove(SHOWING_ON);
     greeting.classList.add(SHOWING_ON);
     greeting.innerText = `Hello ${text}`;
 }
 
-function loadName()
-    const currentUser = localStorage.getItem();
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-        
+        askForName();
     }else{
         paintGreeting(currentUser);
     }
 }
 
 function init(){
-
+    loadName();
 }
 
 init();
