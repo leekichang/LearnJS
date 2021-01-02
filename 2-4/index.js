100e37ec8ec295aeeb152a5326ce104fb37c570c
leekichang <1kichang@yonsei.ac.kr>
2021-01-03 03:52:54 +0900

    Event Listener

---
+++ 2-4/index.js
@@ -0,0 +1,14 @@
+const title = document.querySelector("#title");
+
+function handleResize(event){
+    console.log(event);
+    //console.log("I've been resized");
+}
+
+function handleClick(){
+    title.style.color = "red"
+    //console.log("I've been resized");
+}
+
+window.addEventListener("resize", handleResize)
+title.addEventListener("click", handleClick)
