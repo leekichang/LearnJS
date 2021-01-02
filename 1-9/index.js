e8341d6a2ccd9b891a80337c7ebde9bf8de931e2
leekichang <1kichang@yonsei.ac.kr>
2021-01-03 02:49:23 +0900

    filename fixed and studied usage of Array'

---
+++ 1-9/index.js
@@ -0,0 +1,13 @@
+/*
+const mon = "Mon"
+const tue = "Tue"
+const wed = "Wed"
+const thu = "Thu"
+const fri = "Fri"
+
+console.log(mon, tue, wed, thu, fri)
+효율적이지 못함*/
+
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+console.log(daysOfWeek) //배열 사용하는게 효율적임!
+console.log(daysOfWeek[2])  //배열의 특정값 추출 0부터 indexing
