f42255d4bd0c6c103206899e315e39086692dec2
leekichang <1kichang@yonsei.ac.kr>
2021-01-03 09:57:03 +0900

    Added weather.js and saved user's loaction in local storage

---
+++ JS_Clock/weather.js
@@ -0,0 +1,38 @@
+const COORDS = 'coords';
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude: latitude,
+        longitude: longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+     console.log("Can't acces geo location");
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        //getweather
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
