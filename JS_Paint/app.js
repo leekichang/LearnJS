ced081ecc9d52f75bc9690abd5908ffddfb62c26
leekichang <1kichang@yonsei.ac.kr>
2021-01-05 06:16:04 +0900

    now you can save the save your drawing

--- JS_Paint/app.js
+++ JS_Paint/app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR ="#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -31,7 +34,7 @@ function onMouseMove(event){
     if(!painting){
         ctx.beginPath();
         ctx.moveTo(x,y);
-    }else{
+    }else if(!filling){
         ctx.lineTo(x,y);
         ctx.stroke();
     }
@@ -65,9 +68,19 @@ function handleModeClick(){
 function handleCanvasClick(){
     if(filling){
         ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-    }else{
-        
     }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(event){
+    const image = canvas.toDataURL("iamge/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PainJS[EXPORT]";
+    link.click();
 }
 
 if(canvas){
@@ -76,6 +89,7 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -86,4 +100,8 @@ if(range){
 
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+
+if(mode){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
